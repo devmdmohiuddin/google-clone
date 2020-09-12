faa7b771e6da5841380420dfed6dcae290ab69b3
mohiuddin <mohiuddin0240@gmail.com>
2020-09-12 23:23:57 +0600

    Updates

---
+++ precache-manifest.ebf68be11d21998512c0cbafbb99ce84.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc9043e159576ce70766bcac0f7f1f6a",
+    "url": "/google-clone/index.html"
+  },
+  {
+    "revision": "2f6542aaf6096750f17b",
+    "url": "/google-clone/static/css/main.237748c7.chunk.css"
+  },
+  {
+    "revision": "c8bb9d81ef8b2f54870f",
+    "url": "/google-clone/static/js/2.0faa2f4d.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/google-clone/static/js/2.0faa2f4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f6542aaf6096750f17b",
+    "url": "/google-clone/static/js/main.c34e6863.chunk.js"
+  },
+  {
+    "revision": "53e6f9d7fcbadc32d4de",
+    "url": "/google-clone/static/js/runtime-main.20aa3e5d.js"
+  }
+]);
